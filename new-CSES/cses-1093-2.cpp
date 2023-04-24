a67075125d833b4623fc6c23d29df220fb45e5f7
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-04-24 12:45:49 -0300

    dia 41

---
+++ new-CSES/cses-1093-2.cpp
@@ -0,0 +1,65 @@
+// 24/04/23 // dp com otimizacao de memoria
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <cstring>
+ 
+using namespace std;
+ 
+#define ll long long 
+#define endl "\n"
+ 
+const int MOD = 1e9+7;
+//const int MAXN = 5e2 + 5;
+const int MAXVAL = 62625 + 5;
+
+int solve[5][MAXVAL];
+
+ll expo(ll a, ll b, ll mod=MOD){
+    if(b==0LL) return 1LL;
+    
+    if(b&1LL){
+        return (a*expo(a, b-1LL, mod))%mod;
+    }
+    
+    return expo((a*a)%mod, b/2LL, mod)%mod;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n;
+    cin >> n;
+
+    int sum = (n*(n+1))/2;
+
+    if(sum&1){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    sum /= 2;
+
+    // solve[i][val] = quantas formas eh possivel formar val com os i primeiros numeros
+
+    solve[0][0] = 1;
+
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=sum; j++){
+            solve[i&1][j] = solve[~i&1][j];
+
+            if(j>=i) solve[i&1][j] = (solve[i&1][j] + solve[~i&1][j-i])%MOD;
+        }
+    }
+
+    cout << (solve[n&1][sum] * expo(2, MOD-2, MOD))%MOD << endl;
+
+    return 0;
+}
