b2c73ed6b917d018d50ff6da71dc32d04d41a787
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-05-01 16:23:08 -0300

    dia 48

---
+++ new-CSES/cses-1648.cpp
@@ -0,0 +1,93 @@
+// 01/05/23 // segment tree
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <cstring>
+
+using namespace std;
+
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define ll long long
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const int MAXN = 2e5 + 5;
+
+int n, q;
+ll v[MAXN], seg[3*MAXN]; 
+
+void build(int node, int i, int j){
+    if(i==j){
+        seg[node] = v[i];
+        return;
+    }   
+
+    int mid = (i+j)/2, left=2*node, right=2*node+1;
+    
+    build(left, i, mid);
+    build(right, mid+1, j);
+
+    seg[node] = seg[left] + seg[right];
+}
+
+ll query(int node, int i, int j, int x, int y){
+    if(x<=i && j<=y) return seg[node];
+    
+    else if(j<x || y<i) return 0;
+    
+    int mid = (i+j)/2, left=2*node, right=2*node+1;
+
+    return query(left, i, mid, x, y) + query(right, mid+1, j, x, y);
+}
+
+void update(int node, int i, int j, int k, int u){
+    if(i==j){
+        seg[node] = u;
+        return;
+    }
+    
+    int mid = (i+j)/2, left=2*node, right=2*node+1;
+
+    if(k <= mid){
+        update(left, i, mid, k, u);
+    }
+    else{
+        update(right, mid+1, j, k, u);
+    }
+
+    seg[node] = seg[left] + seg[right];
+}
+
+int main(){
+    fast_io;
+
+    cin >> n >> q;
+
+    for(int i=1; i<=n; i++) cin >> v[i];
+
+    build(1, 1, n);
+
+    while(q--){
+        int t, a, b;
+        cin >> t >> a >> b;
+        
+        if(t==1){ //update
+            update(1, 1, n, a, b);
+        }
+        else{ //query
+            cout << query(1, 1, n, a, b) << endl;
+        }
+    }
+
+    return 0;
+}
--- template_cp.cpp
+++ template_cp.cpp
@@ -1,14 +1,24 @@
-#include <bits/stdc++.h>
+// 01/05/23 //
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <cstring>
 
 using namespace std;
 
 #define endl "\n"
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define pb push_back
-#define mp make_pair
+#define PB push_back
 #define ll long long
 
 typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
 
 const int INF  = 0x3f3f3f3f;
 const int MAXN = (2e5) + 10;
