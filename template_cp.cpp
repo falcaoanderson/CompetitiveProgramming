1e29e62a6b302bf87af929485b74347596153de5
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-05-20 15:38:13 -0300

    dia 67

--- template_cp.cpp
+++ template_cp.cpp
@@ -12,10 +12,13 @@
 
 using namespace std;
 
+#define ll long long
 #define endl "\n"
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 #define PB push_back
-#define ll long long
+#define EB emplace_back
+#define ff first
+#define ss second
 
 typedef pair<int, int> pii;
 typedef tuple<int, int, int> tiii;
