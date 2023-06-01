1edbb5a28516e9b61655e1414b9bf11efed407dd
falcaoanderson <andersonfalcaosantos@gmail.com>
2023-06-01 20:18:24 -0300

    dia 79

---
+++ OBI/obi-2022-hotel.cpp
@@ -0,0 +1,34 @@
+// 01/06/23 //
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define ff first
+#define ss second
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+int main(){
+    fast_io;
+    int d, a, n;
+    cin >> d >> a >> n;
+
+    cout << (31-n+1) * (d + a * min(n-1, 14)) << endl;
+
+    return 0;
+}
