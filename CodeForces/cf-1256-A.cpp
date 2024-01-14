9e94b314f07efda1e8e7ea5babd2dad860e821f8
falcaoanderson <andersonfalcaosantos@gmail.com>
2024-01-13 23:54:09 -0300

    DIA 4

---
+++ CodeForces/cf-1256-A.cpp
@@ -0,0 +1,54 @@
+/*
+13/01/24 
+Problem: 
+Complexity:
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <tuple>
+#include <cstring>
+#include <stack>
+#include <functional>
+#include <cmath>
+
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+int main(){
+    fast_io;
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int a, b, n, s;
+        cin >> a >> b >> n >> s;
+
+        if(s - n * min(s/n, a) <= b) cout << "YES" << endl;
+        else cout << "NO" << endl;   
+    }
+
+    return 0;
+}
