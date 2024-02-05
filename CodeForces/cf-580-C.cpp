ecb1a3d983a3d86c25701d8b6386b88ef55551dd
falcaoanderson <andersonfalcaosantos@gmail.com>
2024-02-05 18:32:06 -0300

    dia 12

--- CodeForces/cf-580-C.cpp
+++ CodeForces/cf-580-C.cpp
@@ -1,9 +1,3 @@
-/*
-05/02/24 
-Problem: 
-Complexity:
-*/
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -27,35 +21,56 @@ using namespace std;
 #define EB emplace_back
 #define FF first
 #define SS second
-#define sz(x) (int)(x).size()
 
 typedef pair<int, int> pii;
 typedef tuple<int, int, int> tiii;
 
 const int INF  = 0x3f3f3f3f;
 const  ll LINF = 0x3f3f3f3f3f3f3f3f;
-const int MAXN = (2e5) + 5;
+const int MAXN = (1e5) + 5;
 const int MOD  = (1e9) + 7;
 
-ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
-    ll r = 1LL;
+bool cat[MAXN];
+vector<int> adj[MAXN];
+int crr[MAXN], max_crr[MAXN];
 
-    while(b>0LL){
-        if(b&1){
-            r = (r*a)%m;
-            b--;
-        }
-        else{
-            a = (a*a)%m;
-            b /= 2LL;
-        }
-    } 
+void dfs(int u, int last){
+    if(cat[u]){
+        crr[u] = crr[last] + 1;
+    }
 
-    return r;
+    max_crr[u] = max(crr[u], max_crr[last]);
+
+    for(int v: adj[u]){
+        if(v != last) dfs(v, u);
+    }
 }
 
 int main(){
     fast_io;
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++) cin >> cat[i];
+
+    for(int i=1; i<n; i++){
+        int a, b;
+        cin >> a >> b;
+
+        adj[a].PB(b);
+        adj[b].PB(a);
+    }
+
+    dfs(1, 0);
+
+    int resp = 0;
+
+    for(int i=2; i<=n; i++){
+        if(adj[i].size()==1 && max_crr[i]<=m) resp++;
+    }
+
+    cout << resp << endl;
 
     return 0;
 }
--- maratona/maratona-2019-subreg-D.cpp
+++ maratona/maratona-2019-subreg-D.cpp
@@ -1,7 +1,7 @@
 /*
-05/02/24 
-Problem: 
-Complexity:
+25/01/24 
+Problem: Denouncing Mafia
+Complexity: O(n+m)
 */
 
 #include <iostream>
@@ -27,35 +27,53 @@ using namespace std;
 #define EB emplace_back
 #define FF first
 #define SS second
-#define sz(x) (int)(x).size()
 
 typedef pair<int, int> pii;
 typedef tuple<int, int, int> tiii;
 
 const int INF  = 0x3f3f3f3f;
 const  ll LINF = 0x3f3f3f3f3f3f3f3f;
-const int MAXN = (2e5) + 5;
+const int MAXN = (1e5) + 5;
 const int MOD  = (1e9) + 7;
 
-ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
-    ll r = 1LL;
+int freq[MAXN], h[MAXN];
+vector<int> adj[MAXN];
 
-    while(b>0LL){
-        if(b&1){
-            r = (r*a)%m;
-            b--;
-        }
-        else{
-            a = (a*a)%m;
-            b /= 2LL;
-        }
-    } 
+void dfs(int u){
+    for(int v: adj[u]){
+        dfs(v);
 
-    return r;
+        h[u] = max(h[u], h[v]);
+    }
+
+    if(h[u] != 0) freq[h[u]]--;
+
+    h[u]++;
+    freq[h[u]]++;
 }
 
 int main(){
     fast_io;
+
+    int n, k;
+    cin >> n >> k;
+
+    for(int i=2; i<=n; i++){
+        int a;
+        cin >> a;
+
+        adj[a].PB(i);
+    }
+
+    dfs(1);
+
+    int resp=0;
+    for(int i=n; i>=1 && k>0; i--){
+        resp += i * min(k, freq[i]);
+        k -= min(k, freq[i]);
+    }
+
+    cout << resp << endl;
 
     return 0;
 }
