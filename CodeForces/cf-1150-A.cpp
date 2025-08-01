6f36bc4f900357907f26d6f07b49c8aad266bbd9
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-07-31 23:56:08 -0300

    DIA 21

---
+++ CodeForces/cf-1150-A.cpp
@@ -0,0 +1,72 @@
+/*
+00/07/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define sz(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
+    ll r = 1LL;
+
+    while(b>0LL){
+        if(b&1){
+            r = (r*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b /= 2LL;
+        }
+    } 
+
+    return r;
+}
+
+int main(){
+    fast_io;
+
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    int buy=1000, sell=0;
+
+    for(int i=0; i<n; i++){
+        int x;
+        cin >> x;
+
+        buy = min(buy, x);
+    }
+    
+    for(int i=0; i<m; i++){
+        int x;
+        cin >> x;
+
+        sell = max(sell, x);
+    }
+
+    if(sell>buy){
+        r = r%buy + (r/buy) * sell;
+    }
+
+    cout << r << endl;
+
+    return 0;
+}
