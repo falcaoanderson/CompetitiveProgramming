aefa5ce760ca95a0e7386f8733fecfd1685c8232
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-08-05 16:30:13 -0300

    DIA 25

---
+++ CodeForces/cf-1139-A.cpp
@@ -0,0 +1,45 @@
+/*
+04/08/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define sz(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+int main(){
+    fast_io;
+
+    int n;
+    cin >> n;
+    int resp =0;
+    for(int i=0; i<n; i++){
+        char c;
+        cin >> c;
+        int x = c - '0';
+
+        if(x%2==0){
+            resp += i+1;
+        }
+    }
+
+    cout << resp << endl;
+
+    return 0;
+}
