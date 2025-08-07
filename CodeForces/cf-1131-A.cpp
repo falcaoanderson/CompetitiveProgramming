30980a365ca8e1d97d166f4d588401a44f10258d
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-08-07 20:06:06 -0300

    DIA 28

---
+++ CodeForces/cf-1131-A.cpp
@@ -0,0 +1,52 @@
+/*
+07/08/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define sz(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
+    ll r = 1LL;
+
+    while(b>0LL){
+        if(b&1){
+            r = (r*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b /= 2LL;
+        }
+    } 
+
+    return r;
+}
+
+int main(){
+    fast_io;
+
+    int w1, h1, w2, h2;
+    cin >> w1 >> h1 >> w2 >> h2;
+
+    cout << 2 * (h1 + h2 + max(w1, w2)) + 4<< endl;
+
+    return 0;
+}
