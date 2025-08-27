be593a0cf9499b0e0e28042b47521e12be14ed64
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-08-26 21:29:54 -0300

    DIA 46

---
+++ CodeForces/cf-282-A.cpp
@@ -0,0 +1,42 @@
+/*
+25/08/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define SZ(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+int main(){
+    fast_io;
+
+    int n;
+    cin >> n;
+    int resp = 0;
+    for(int i=0; i<n; i++){
+        string s;
+        cin >> s;
+
+        if(s=="++X" || s=="X++") resp++;
+        else resp--;
+    }
+    cout << resp << endl;
+
+    return 0;
+}
