b167db2fe69d150bbc9b5f0597a27ca882ec466b
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-08-30 19:58:20 -0300

    DIA 50

---
+++ CodeForces/cf-158-A.cpp
@@ -0,0 +1,58 @@
+/*
+29/08/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define SZ(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
+    ll r = 1LL;
+
+    while(b>0LL){
+        if(b&1){
+            r = (r*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b /= 2LL;
+        }
+    } 
+
+    return r;
+}
+
+int main(){
+    fast_io;
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    for(int i=0; i<n; i++) cin >> v[i];
+    
+    int resp = 0;
+    while(resp<n && v[resp]>0 && v[resp]>=v[k-1]) resp++;
+
+    cout << resp << endl;
+
+    return 0;
+}
