f9ebcbe03c626b7202f04be6b3dc963744080692
falcaoanderson <andersonfalcaosantos@gmail.com>
2025-11-03 17:34:14 -0300

    DIA 71

---
+++ CodeForces/div2_1087/A.cpp
@@ -0,0 +1,70 @@
+/*
+08/10/25 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define SZ(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
+    ll r = 1LL;
+
+    while(b>0LL){
+        if(b&1){
+            r = (r*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b /= 2LL;
+        }
+    } 
+
+    return r;
+}
+
+int main(){
+    fast_io;
+
+    string s;
+    cin >> s;
+
+    int l, r, n=SZ(s);
+    l = r = (n-1)/2;
+
+    while(0<=l || r<n){
+        if(l==r){
+            cout << s[l];
+            l--;
+            r++;
+        }
+        else if((r-l)&1){
+            cout << s[l];
+            l--;
+        }
+        else{
+            cout << s[r];
+            r++;
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
