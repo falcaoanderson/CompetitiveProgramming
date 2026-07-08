a1205173258400908bf81bb3c238139e63a26518
falcaoanderson <andersonfalcaosantos@gmail.com>
2026-07-08 17:27:53 -0300

    [2026] DIA 21

---
+++ CodeForces/div2_2203/A.cpp
@@ -0,0 +1,61 @@
+/*
+25/02/26 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define endl "\n"
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PB push_back
+#define EB emplace_back
+#define FF first
+#define SS second
+#define SZ(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tiii;
+
+const int INF  = 0x3f3f3f3f;
+const  ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int MAXN = (2e5) + 5;
+const int MOD  = (1e9) + 7;
+
+ll exp(ll a, ll b, ll m=MOD){ // 0^0 = 1
+    ll r = 1LL;
+
+    while(b>0LL){
+        if(b&1){
+            r = (r*a)%m;
+            b--;
+        }
+        else{
+            a = (a*a)%m;
+            b /= 2LL;
+        }
+    } 
+
+    return r;
+}
+
+void solve(){
+    int n, m, d;
+    cin >> n >> m >> d;
+
+    int q = ((d / m) + 1);
+    cout << n / q + (int)((n % q) != 0) << endl;
+}
+
+int main(){
+    fast_io;
+
+    int t=1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
